bb5e177b758c6ac807ca2a097d7b978a6cb99f1f
Jean-Brice <jeanbrice.danho@student-cs.fr>
2026-02-05 16:14:40 +0100

    Ecriture de index.js

---
+++ app/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const { Pool } = require("pg");
+const redis = require("redis");
+
+const app = express();
+app.use(express.json());
+
+// PostgreSQL
+const pgClient = new Pool({
+  host: "postgres",
+  user: "postgres",
+  password: "postgres",
+  database: "testdb",
+});
+
+// Redis
+const redisClient = redis.createClient({
+  socket: {
+    host: "redis",
+    port: 6379,
+  },
+});
+redisClient.connect();
+
+app.get("/", (req, res) => {
+  res.send("Multi-container app running 🚀");
+});
+
+app.get("/users", async (req, res) => {
+  const cache = await redisClient.get("users");
+  if (cache) {
+    return res.json(JSON.parse(cache));
+  }
+
+  const result = await pgClient.query("SELECT * FROM users");
+  await redisClient.set("users", JSON.stringify(result.rows), { EX: 60 });
+  res.json(result.rows);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
